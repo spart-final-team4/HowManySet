9ca2ef06d0f6f686de2b6df0a107e6bafa8db6b1
EllGHT <rmsgh1188@gmail.com>
2025-06-03 21:40:49 +0900

    feat: #6 - MyPageCoordinator 구현
    vc, reactor 세팅

--- HowManySet/Presentation/Feature/MyPage/MyPageViewController.swift
+++ HowManySet/Presentation/Feature/MyPage/MyPageViewController.swift
@@ -5,4 +5,22 @@
 //  Created by 정근호 on 5/30/25.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class MyPageViewController: UIViewController {
+    
+    private let reactor: MyPageReactor
+    
+    init(reactor: MyPageReactor) {
+        self.reactor = reactor
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- HowManySet/Presentation/Feature/MyPage/Reactor/MyPageViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  MyPageViewModel.swift
-//  HowManySet
-//
-//  Created by 정근호 on 5/30/25.
-//
-
-import Foundation
