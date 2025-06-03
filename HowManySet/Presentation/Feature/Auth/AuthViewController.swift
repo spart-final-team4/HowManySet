ac0709e22b8b7371eb6f5b15c7e1d5afa45b6709
EllGHT <rmsgh1188@gmail.com>
2025-06-03 21:11:19 +0900

    feat: #6 - AuthCoordinator 구현
    vc 생성 시 reactor 주입

--- HowManySet/Presentation/Feature/Auth/AuthViewController.swift
+++ HowManySet/Presentation/Feature/Auth/AuthViewController.swift
@@ -9,4 +9,14 @@ import UIKit
 
 final class AuthViewController: UIViewController {
     
+    private var reactor: AuthViewReactor
+    
+    init(reactor: AuthViewReactor) {
+        self.reactor = reactor
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
