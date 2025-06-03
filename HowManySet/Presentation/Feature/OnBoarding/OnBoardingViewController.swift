88984b1586840ff141846ce89b7b2cab2158d5aa
EllGHT <rmsgh1188@gmail.com>
2025-06-03 20:59:23 +0900

    feat: #6 - OnBoardingCoordinator 구현
    vc 생성 시 reactor 주입

--- HowManySet/Presentation/Feature/OnBoarding/OnBoardingViewController.swift
+++ HowManySet/Presentation/Feature/OnBoarding/OnBoardingViewController.swift
@@ -5,4 +5,19 @@
 //  Created by 정근호 on 6/3/25.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class OnBoardingViewController: UIViewController {
+    
+    private var reactor: OnBoardingViewReactor
+    
+    init(reactor: OnBoardingViewReactor) {
+        self.reactor = reactor
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
