c0748ff62102d24905f0b0e4c36ff427a1d7c8ec
EllGHT <rmsgh1188@gmail.com>
2025-06-03 21:01:34 +0900

    feat: #6 - AppCoordinator 구현

--- HowManySet/App/Coordinator/AppCoordinator.swift
+++ HowManySet/App/Coordinator/AppCoordinator.swift
@@ -7,6 +7,73 @@
 
 import UIKit
 
-final class AppCoordinator {
+final class AppCoordinator: Coordinator {
     
+    var window: UIWindow
+    private var childCoordinators: [Coordinator] = []
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+
+    func start() {
+        // 앱 시작 시 로그인 여부, 온보딩 완료 여부 등을 확인
+        // true값 추후 수정
+        let isLoggedIn = true // ... (Firebase Auth, UserDefaults 등 확인)
+        let hasCompletedOnboarding = true // ... (UserDefaults 등 확인)
+
+        if !hasCompletedOnboarding {
+            showOnboardingFlow()
+        } else if !isLoggedIn {
+            showAuthFlow()
+        } else {
+            showTabBarFlow()
+        }
+    }
+    
+    private func showOnboardingFlow() {
+        let onBoardingCoordinator = OnBoardingCoordinator(navigationController: UINavigationController()) // 온보딩 전용 Navigation
+        onBoardingCoordinator.finishFlow = { [weak self, weak onBoardingCoordinator] in
+            // 온보딩 완료 후 처리 (예: 온보딩 완료 상태 저장, 로그인 흐름으로 전환)
+            guard let onBoardingCoordinator, let self else { return }
+            self.childDidFinish(onBoardingCoordinator)
+            onBoardingCoordinator.completeOnBoarding()
+            self.showAuthFlow()
+        }
+        onBoardingCoordinator.start()
+        
+        window.rootViewController = onBoardingCoordinator.navigationController
+        window.makeKeyAndVisible()
+    }
+
+    private func showAuthFlow() {
+        let authCoordinator = AuthCoordinator(navigationController: UINavigationController()) // 로그인/회원가입 전용 Navigation
+        authCoordinator.finishFlow = { [weak self, weak authCoordinator] in
+            // 로그인/회원가입 완료 후 처리 (예: 로그인 상태 저장, 메인 흐름으로 전환)
+            guard let authCoordinator, let self else { return }
+            self.childDidFinish(authCoordinator)
+            self.showTabBarFlow()
+        }
+        authCoordinator.start()
+        
+        window.rootViewController = authCoordinator.navigationController
+        window.makeKeyAndVisible()
+    }
+
+    private func showTabBarFlow() {
+        let tabBarCoordinator = TabBarCoordinator(tabBarController: UITabBarController()) // 메인 탭바
+    
+        tabBarCoordinator.start()
+        
+        window.rootViewController = tabBarCoordinator.tabBarController
+        window.makeKeyAndVisible()
+    }
+    
+    private func childDidFinish(_ child: Coordinator) {
+        for (index, coordinator) in childCoordinators.enumerated() {
+            if ObjectIdentifier(coordinator) == ObjectIdentifier(child) {
+                childCoordinators.remove(at: index)
+            }
+        }
+    }
 }
