3b1a0b926a2d9e28ec7fbb9e05e1dbc027172b6f
EllGHT <rmsgh1188@gmail.com>
2025-06-04 15:56:46 +0900

    feat: #6 - 편집 옵션 뷰, 메모 뷰 세팅
    홈에서 ... 버튼 클릭 시 필요

---
+++ HowManySet/Presentation/Feature/Home/MemoView.swift
@@ -0,0 +1,21 @@
+//
+//  MemoView.swift
+//  HowManySet
+//
+//  Created by 정근호 on 6/4/25.
+//
+
+import UIKit
+import SnapKit
+
+final class MemoView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
