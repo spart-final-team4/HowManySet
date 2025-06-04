7d7d3a6a0e53566456deedf3877fdb0d218e57b8
EllGHT <rmsgh1188@gmail.com>
2025-06-04 15:43:31 +0900

    feat: #6 - EditRoutineVC, Reactor 세팅

--- HowManySet/Presentation/Feature/EditRoutine/EditRoutineViewController.swift
+++ HowManySet/Presentation/Feature/EditRoutine/EditRoutineViewController.swift
@@ -5,4 +5,24 @@
 //  Created by 정근호 on 5/30/25.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class EditRoutineViewController: UIViewController {
+    
+    private let reactor: EditRoutinViewReactor
+    
+    init(reactor: EditRoutinViewReactor) {
+        self.reactor = reactor
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
