51d9f95204e55b64fceec05ea677063b1de723c0
UkhyunGo <dnrwk99@gmail.com>
2025-06-05 09:59:06 +0900

    feat: #2 - AppDelegate KakaoSDK 로그인 관련 코드 구현

--- HowManySet/App/AppDelegate.swift
+++ HowManySet/App/AppDelegate.swift
@@ -9,6 +9,8 @@ import UIKit
 import FirebaseCore
 import FirebaseAuth
 import GoogleSignIn
+import KakaoSDKCommon
+import KakaoSDKAuth
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -20,31 +22,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         FirebaseApp.configure()
         
+        // Kakao SDK 초기화 (앱 키는 info.plist 등에서 안전하게 관리) ->
+        KakaoSDK.initSDK(appKey: "NATIVE_APP_KEY")
+        
         return true
     }
-
-    // MARK: 구글 인증 과정에서 앱으로 돌아오는 URL을 처리
-    func application(_ app: UIApplication,
-                     open url: URL,
-                     options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-      return GIDSignIn.sharedInstance.handle(url)
-    }
     
+    // 구글/카카오 인증 결과 처리 (iOS 13 이상, SceneDelegate 병행 사용)
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:] ) -> Bool {
+        // 구글 로그인 처리
+        if GIDSignIn.sharedInstance.handle(url) {
+            return true
+        }
+        // 카카오 로그인 처리
+        if AuthApi.isKakaoTalkLoginUrl(url) {
+            return AuthController.handleOpenUrl(url: url)
+        }
+        return false
+    }
     
     // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        // 기본 SceneDelegate 사용
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    
+    func application(
+        _ application: UIApplication,
+        didDiscardSceneSessions sceneSessions: Set<UISceneSession>
+    ) {
+        // 필요시 리소스 해제 등 처리
     }
-
-
 }
-
