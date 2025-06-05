0134dff53739284516bcf41aa8237f8294b34f61
EllGHT <rmsgh1188@gmail.com>
2025-06-05 12:34:03 +0900

    feat: #6 - 언어 변경 설정 이동 메서드 구현

--- HowManySet/App/Coordinator/MyPageCoordinator.swift
+++ HowManySet/App/Coordinator/MyPageCoordinator.swift
@@ -26,4 +26,24 @@ final class MyPageCoordinator: MyPageCoordinatorProtocol {
         
         navigationController.pushViewController(myPageVC, animated: true)
     }
+    /// 언어 변경 처리 (설정 앱 이동 알림)
+    func presentLanguageSettingAlert(from viewController: UIViewController) {
+        let alert = UIAlertController(
+            title: "언어 변경",
+            message: "언어는 설정 앱에서 변경할 수 있어요.\n앱 설정으로 이동할까요?",
+            preferredStyle: .alert
+        )
+
+        alert.addAction(UIAlertAction(title: "이동", style: .default) { _ in
+            // 앱 설정 화면으로 이동
+            if let url = URL(string: UIApplication.openSettingsURLString),
+               UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        })
+
+        alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
+
+        viewController.present(alert, animated: true)
+    }
 }
