cb8465da6d19f1b7fc108c9af53d536a2ede5e1a
EllGHT <rmsgh1188@gmail.com>
2025-06-05 12:29:27 +0900

    feat: #6 - 홈, 캘린더, 루틴리스트, 운동완료 뷰 내 세부전환 메서드 구현

--- HowManySet/App/Coordinator/HomeCoordinator.swift
+++ HowManySet/App/Coordinator/HomeCoordinator.swift
@@ -27,15 +27,46 @@ final class HomeCoordinator: HomeCoordinatorProtocol {
         navigationController.pushViewController(homeVC, animated: true)
     }
     
+    /// 초기 빈 화면에서 +버튼 클릭시 present
+    func presentRoutineListView() {
+        let routineListCoordinator = RoutineListCoordinator(navigationController: navigationController, container: container)
+        
+        routineListCoordinator.startModal()
+    }
+    
+    /// 운동 종목 뷰의 메뉴 버튼 클릭시 present
     func presentWorkoutOptionView() {
+        let reactor = WorkoutOptionViewReactor()
+        let workoutOptionVC = WorkoutOptionViewController(reactor: reactor)
         
+        // medium 크기의 bottomSheet로 설정
+        if let sheet = workoutOptionVC.sheetPresentationController {
+            sheet.detents = [.medium()]
+            sheet.prefersGrabberVisible = true
+        }
+        
+        navigationController.present(workoutOptionVC, animated: true)
     }
     
-    func navigateToEditRoutineView() {
+    /// presentWorkoutOptionView에서 루틴 수정 버튼 클릭 시 push or present
+    func pushEditRoutineView() {
+        let reactor = EditRoutinViewReactor()
+        let editRoutineVC = EditRoutineViewController(reactor: reactor)
         
+        // present로 할지 push로 할지 결정 필요
+//        if let sheet = editRoutineVC.sheetPresentationController {
+//            sheet.detents = [.large()]
+//            sheet.prefersGrabberVisible = true
+//        }
+//        
+//        navigationController.present(editRoutineVC, animated: true)
+        navigationController.pushViewController(editRoutineVC, animated: true)
     }
     
-    func navigateToRoutineCompleteView() {
+    /// 운동 완료 페이지로 이동
+    func pushRoutineCompleteView() {        
+        let routineCompleteCoordinator = RoutineCompleteCoordinator(navigationController: navigationController, container: container)
         
+        routineCompleteCoordinator.start()
     }
 }
