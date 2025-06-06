6a12771cee5102924fb18dd83f84385e82d805b0
EllGHT <rmsgh1188@gmail.com>
2025-06-06 10:37:21 +0900

    feat: #1 - toWorkOutTimeLabel Extension 추가 및 적용

--- HowManySet/Presentation/Common/Extension/Int+Extension.swift
+++ HowManySet/Presentation/Common/Extension/Int+Extension.swift
@@ -17,4 +17,18 @@ extension Int {
         
         return String(format: "%02d:%02d", minute, seconds)
     }
+    
+    /// Int 값을 00:00:00 형식의 시:분:초 String으로 나타냄
+    /// LiveActivity에서도 사용되어 Target 추가되어 있음
+    func toWorkOutTimeLabel() -> String {
+        let hour = self / 3600
+        let minute = (self % 3600) / 60
+        let second = self % 60
+        
+        if hour > 0 {
+            return String(format: "%02d:%02d:%02d", hour, minute, second)
+        } else {
+            return String(format: "%02d:%02d", minute, second)
+        }
+    }
 }
