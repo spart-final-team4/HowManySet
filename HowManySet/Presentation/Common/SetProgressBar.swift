e0c254cacfe749ee632be21c87ee83aea1e49279
EllGHT <rmsgh1188@gmail.com>
2025-06-06 10:45:46 +0900

    docs: #1 - 재사용 SetProgressBar 파일 생성
    LiveActivity에서도 사용되기에 target 추가

---
+++ HowManySet/Presentation/Common/SetProgressBar.swift
@@ -0,0 +1,28 @@
+//
+//  SetProgressBar.swift
+//  HowManySet
+//
+//  Created by 정근호 on 6/6/25.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+
+final class SetProgressBarView: UIView {
+    
+    // MARK: - Properties
+    
+    // MARK: - UI Components
+    
+    // MARK: - Initializer
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
