dea112b4f4c0a4a14a82360ecfd6f03c23477780
EllGHT <rmsgh1188@gmail.com>
2025-06-07 23:10:58 +0900

    docs: #18 - PagingView, 홈 초기 화면 뷰 생성

---
+++ HowManySet/Presentation/Feature/Home/HomePagingView.swift
@@ -0,0 +1,28 @@
+//
+//  HomeView.swift
+//  HowManySet
+//
+//  Created by 정근호 on 6/7/25.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class HomePagingView: UIView {
+    
+    // MARK: - Properties
+    
+    // MARK: - UI Components
+    
+    // MARK: - Initializer
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
