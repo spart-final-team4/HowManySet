8ec7f40837e7f7522325f6f6cacff27d9eef9f25
minjae <qlqlzbto@naver.com>
2025-06-09 20:20:19 +0900

    feat: #14 - RepositoryImpl, RecordRepository 수정

--- HowManySet/Data/Repositories/RoutineRepositoryImpl.swift
+++ HowManySet/Data/Repositories/RoutineRepositoryImpl.swift
@@ -13,16 +13,25 @@ import RxSwift
 /// 실제 데이터 소스(예: 데이터베이스, 네트워크 등)와 연동하여 운동 루틴을 조회, 저장, 수정, 삭제하는 기능을 제공합니다.
 final class RoutineRepositoryImpl: RoutineRepository {
     
+    private let realmService: RealmServiceProtocol
+    
+    init(realmService: RealmServiceProtocol) {
+        self.realmService = realmService
+    }
+    
     /// 주어진 사용자 ID에 해당하는 운동 루틴 리스트를 비동기적으로 조회합니다.
     ///
     /// - Parameter uid: 운동 루틴을 조회할 사용자의 고유 식별자
     /// - Returns: `Single`로 감싸진 `WorkoutRoutine` 배열, 조회 성공 시 배열을 방출하고 실패 시 에러를 방출합니다.
     func fetchRoutine(uid: String) -> Single<[WorkoutRoutine]> {
-        return Single.create { observer in
-            // TODO: 운동 루틴 조회 구현
+        return Single.create { [weak self] observer in
+            guard let routines = self?.realmService.read(type: .workoutRecord) as? [WorkoutRoutineDTO] else {
+                observer(.failure(RealmErrorType.dataNotFound))
+                return Disposables.create()
+            }
             
             // 예시: 빈 배열 반환
-            observer(.success([]))
+            observer(.success(routines.map{$0.toEntity()}))
             
             return Disposables.create()
         }
@@ -34,7 +43,8 @@ final class RoutineRepositoryImpl: RoutineRepository {
     ///   - uid: 운동 루틴을 삭제할 사용자의 고유 식별자
     ///   - item: 삭제할 `WorkoutRoutine` 객체
     func deleteRoutine(uid: String, item: WorkoutRoutine) {
-        // TODO: 운동 루틴 삭제 구현
+        let routine = WorkoutRoutineDTO(entity: item)
+        realmService.delete(item: routine)
     }
     
     /// 주어진 사용자 ID에 해당하는 운동 루틴을 저장합니다.
@@ -43,7 +53,8 @@ final class RoutineRepositoryImpl: RoutineRepository {
     ///   - uid: 운동 루틴을 저장할 사용자의 고유 식별자
     ///   - item: 저장할 `WorkoutRoutine` 객체
     func saveRoutine(uid: String, item: WorkoutRoutine) {
-        // TODO: 운동 루틴 저장 구현
+        let routine = WorkoutRoutineDTO(entity: item)
+        realmService.create(item: routine)
     }
     
     /// 주어진 사용자 ID에 해당하는 운동 루틴을 수정합니다.
@@ -53,6 +64,22 @@ final class RoutineRepositoryImpl: RoutineRepository {
     ///   - item: 수정할 `WorkoutRoutine` 객체
     func updateRoutine(uid: String, item: WorkoutRoutine) {
         // TODO: 운동 루틴 수정 구현
+        
+        let routines = realmService.read(type: .workoutRoutine)
+        guard let array = routines as? [WorkoutRoutineDTO] else { return }
+        
+        var index = 0
+        for i in 0..<array.count {
+            if array[i].name == item.name {
+                index = i
+                break
+            }
+        }
+        realmService.update(item: routines![index]) { routine in
+            let data = routine as! WorkoutRoutineDTO
+            data.name = item.name
+            data.workoutArray = item.workouts.map{ WorkoutDTO(entity: $0) }
+        }
     }
 }
 
