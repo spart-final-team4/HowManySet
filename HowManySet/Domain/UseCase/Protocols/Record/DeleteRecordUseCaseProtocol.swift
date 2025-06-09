452f5ea9b43d38b688a1ac5ba06aa409dfa5c55f
minjae <qlqlzbto@naver.com>
2025-06-09 21:29:36 +0900

    feat: #14 - DeleteRecordUseCase, DeleteAllRecordUseCase 생성 및 구현

---
+++ HowManySet/Domain/UseCase/Protocols/Record/DeleteRecordUseCaseProtocol.swift
@@ -0,0 +1,19 @@
+//
+//  DeleteRecordUseCaseProtocol.swift
+//  HowManySet
+//
+//  Created by MJ Dev on 6/9/25.
+//
+
+import Foundation
+
+/// 운동 기록을 삭제하는 유스케이스를 정의한 프로토콜입니다.
+/// 비즈니스 로직 계층에서 특정 운동 기록을 삭제하는 책임을 가집니다.
+protocol DeleteRecordUseCaseProtocol {
+    
+    /// 특정 운동 기록을 삭제합니다.
+    /// - Parameters:
+    ///   - uid: 사용자 식별자
+    ///   - item: 삭제할 운동 기록
+    func execute(uid: String, item: WorkoutRecord)
+}
