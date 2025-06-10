7d04114d53076e3f4a7b9b5698b9c662091805eb
Sn8Ch0 <whtjsdn98@gmail.com>
2025-06-10 12:39:25 +0900

    feat: #5 - Calendar Reactor 구현

--- HowManySet/Presentation/Feature/Calendar/Reactor/CalendarViewReactor.swift
+++ HowManySet/Presentation/Feature/Calendar/Reactor/CalendarViewReactor.swift
@@ -7,36 +7,60 @@ final class CalendarViewReactor: Reactor {
     private let saveRecordUseCase: SaveRecordUseCase
     private let fetchRecordUseCase: FetchRecordUseCase
 
-    // Action is an user interaction
+    // MARK: - Action is an user interaction
     enum Action {
-
+        case selectDate(Date)
     }
 
-    // Mutate is a state manipulator which is not exposed to a view
+    // MARK: - Mutate is a state manipulator which is not exposed to a view
     enum Mutation {
-
+        case setSelectedDate(Date)
+        case setRecords([WorkoutRecord])
     }
 
-    // State is a current view state
+    // MARK: - State is a current view state
     struct State {
-
+        var selectedDate: Date = Date()
+        var records: [WorkoutRecord] = []
     }
 
     let initialState: State
 
+    // MARK: - Init
     init(saveRecordUseCase: SaveRecordUseCase, fetchRecordUseCase: FetchRecordUseCase) {
         self.saveRecordUseCase = saveRecordUseCase
         self.fetchRecordUseCase = fetchRecordUseCase
         self.initialState = State()
     }
 
-    //    // Action -> Mutation
-    //    func mutate(action: Action) -> Observable<Mutation> {
-    //
-    //    }
-    //
-    //    // Mutation -> State
-    //    func reduce(state: State, mutation: Mutation) -> State {
-    //
-    //    }
+    // MARK: - Action -> Mutation
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case let .selectDate(date):
+            // 실제 fetch 대신 mockData 사용
+            let calendar = Calendar.current
+            let filteredRecords = WorkoutRecord.mockData.filter {
+                calendar.isDate($0.date, inSameDayAs: date)
+            }
+
+            return Observable.concat([
+                Observable.just(.setSelectedDate(date)),
+                Observable.just(.setRecords(filteredRecords))
+            ])
+        }
+    }
+
+    // MARK: - Mutation -> State
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+
+        switch mutation {
+        case let .setSelectedDate(date):
+            newState.selectedDate = date
+        case let .setRecords(records):
+            newState.records = records
+        }
+
+        return newState
+    }
 }
