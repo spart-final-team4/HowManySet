5f7b048a7eed8925edbe50d0e98349f13cb8776d
Sn8Ch0 <whtjsdn98@gmail.com>
2025-06-13 19:55:18 +0900

    refactor: #50 - 스택 뷰 제거하고, 버튼을 테이블 뷰의 FooterView로 수정

--- HowManySet/Presentation/Feature/RoutineList/RoutineListView.swift
+++ HowManySet/Presentation/Feature/RoutineList/RoutineListView.swift
@@ -3,11 +3,14 @@ import SnapKit
 import Then
 
 final class RoutineListView: UIView {
+    // MARK: - Properties
     private let titleLabel = UILabel()
     private let addNewRoutineButton = UIButton()
     private let routineTableView = UITableView()
-    private let stackView = UIStackView()
+    // FooterView 구성
+    private let footerView = UIView()
 
+    // MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupUI()
@@ -29,6 +32,7 @@ private extension RoutineListView {
         setAppearance()
         setViewHierarchy()
         setConstraints()
+        setupFooterView()
     }
 
     func setAppearance() {
@@ -44,39 +48,57 @@ private extension RoutineListView {
         }
 
         addNewRoutineButton.do {
-            $0.backgroundColor = .disabledButton
+            $0.backgroundColor = .brand // 임의로 색상 변경함‼️
             $0.setTitle("새 루틴 구성", for: .normal)
             $0.titleLabel?.font = .systemFont(ofSize: 18, weight: .regular)
-            $0.tintColor = .white
+            $0.setTitleColor(.black, for: .normal) // 임의로 색상 변경함‼️
             $0.layer.cornerRadius = 12
             clipsToBounds = true
         }
 
-        stackView.do {
-            $0.axis = .vertical
-            $0.spacing = 20
-            $0.alignment = .fill
-            $0.distribution = .fill
+        footerView.do {
+            $0.backgroundColor = .clear
         }
     }
 
     func setViewHierarchy() {
-        addSubview(stackView)
+        footerView.addSubview(addNewRoutineButton)
 
-        [titleLabel, routineTableView, addNewRoutineButton].forEach {
-            stackView.addArrangedSubview($0)
-        }
+        [
+            titleLabel,
+            routineTableView
+        ].forEach { addSubview($0) }
     }
 
     func setConstraints() {
-        stackView.snp.makeConstraints {
+        titleLabel.snp.makeConstraints {
             $0.top.equalTo(safeAreaLayoutGuide)
             $0.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(20)
-            $0.bottom.lessThanOrEqualTo(safeAreaLayoutGuide).inset(20)
+        }
+
+        routineTableView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(12)
+            $0.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(20)
+            $0.bottom.equalTo(safeAreaLayoutGuide)
         }
 
         addNewRoutineButton.snp.makeConstraints {
-            $0.height.equalTo(safeAreaLayoutGuide.snp.height).multipliedBy(0.07)
+            $0.top.equalToSuperview().offset(20)
+            $0.horizontalEdges.equalToSuperview()
+            $0.bottom.equalToSuperview().inset(20)
+            $0.height.equalTo(56)
         }
+    }
+
+    // MARK: - FooterView Setting
+    /// FooterView를 setup하는 메서드
+    func setupFooterView() {
+        // FooterView 높이 고정
+        let targetSize = CGSize(width: UIScreen.main.bounds.width, height: 96)
+        let fittingSize = footerView.systemLayoutSizeFitting(targetSize)
+        footerView.frame.size = fittingSize
+
+        // 테이블 뷰에 적용
+        routineTableView.tableFooterView = footerView
     }
 }
