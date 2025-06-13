4bae4487c30c68b517693fde3e94b414a36091c8
Sn8Ch0 <whtjsdn98@gmail.com>
2025-06-13 19:56:29 +0900

    refactor: #50 - TableView DiffableDataSource 사용하여 mockData 출력

--- HowManySet/Presentation/Feature/RoutineList/RoutineListViewController.swift
+++ HowManySet/Presentation/Feature/RoutineList/RoutineListViewController.swift
@@ -5,11 +5,21 @@ import ReactorKit
 import SnapKit
 
 final class RoutineListViewController: UIViewController, View {
+    // MARK: - Properties
     var disposeBag = DisposeBag()
 
     let routineListView = RoutineListView()
     private weak var coordinator: RoutineListCoordinatorProtocol?
 
+    // DiffableDataSource 정의
+    private var dataSource: UITableViewDiffableDataSource<Section, WorkoutRoutine>!
+
+    // Section 정의
+    enum Section {
+        case main
+    }
+
+    // MARK: - Init
     init(reactor: RoutineListViewReactor, coordinator: RoutineListCoordinatorProtocol) {
         super.init(nibName: nil, bundle: nil)
         self.reactor = reactor
@@ -20,6 +30,7 @@ final class RoutineListViewController: UIViewController, View {
         fatalError("init(coder:) has not been implemented")
     }
 
+    // MARK: - LifeCycle
     override func loadView() {
         view = routineListView
     }
@@ -28,8 +39,9 @@ final class RoutineListViewController: UIViewController, View {
         super.viewDidLoad()
         setDelegates()
         setRegisters()
-
-        reactor?.action.onNext(.viewDidLoad)
+        configureDataSource()
+        // MockData 표시
+        applySnapshot(with: WorkoutRoutine.mockData)
     }
 
     // MARK: - Bind
@@ -37,6 +49,7 @@ final class RoutineListViewController: UIViewController, View {
         // routines -> tableView 바인딩
         reactor.state
             .map(\.routines)
+            .distinctUntilChanged() // optional 같은 값 방지
             .bind(to: routineListView.publicRoutineTableView.rx.items(
                 cellIdentifier: RoutineCell.identifier,
                 cellType: RoutineCell.self
@@ -56,12 +69,45 @@ private extension RoutineListViewController {
     func setRegisters() {
         routineListView.publicRoutineTableView.register(RoutineCell.self, forCellReuseIdentifier: RoutineCell.identifier)
     }
+
+    func configureDataSource() {
+        dataSource = UITableViewDiffableDataSource<Section, WorkoutRoutine>(
+            tableView: routineListView.publicRoutineTableView
+        ) { tableView, indexPath, routine in
+            guard let cell = tableView.dequeueReusableCell(
+                withIdentifier: RoutineCell.identifier,
+                for: indexPath
+            ) as? RoutineCell else {
+                return UITableViewCell()
+            }
+
+            // 데이터를 cell에 주입
+            cell.configure(with: routine)
+            return cell
+        }
+    }
+}
+
+// MARK: - Snapshot
+extension RoutineListViewController {
+    /// 현재 보여줄 데이터 전체 상태를 전달하는 메서드
+    func applySnapshot(with routines: [WorkoutRoutine], animatingDifferences: Bool = true) {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, WorkoutRoutine>()
+        // Section 추가
+        snapshot.appendSections([.main])
+        // Item 추가
+        snapshot.appendItems(routines, toSection: .main)
+        // 실제 적용
+        dataSource.apply(snapshot, animatingDifferences: animatingDifferences)
+    }
 }
 
 // MARK: - Calendar buttons addTarget
 extension RoutineListViewController: UITableViewDelegate {
     /// TableView Cell의 높이를 설정하는 메서드
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        UIScreen.main.bounds.height * 0.13
+        /* height를 기본값으로 지정하는 이유
+         => 다른 기기에서 셀 내부의 내용이 다 잘림. 어차피 스크롤이 되기 때문에 기본값으로 지정해줘도 괜찮다고 생각함. */
+        132
     }
 }
