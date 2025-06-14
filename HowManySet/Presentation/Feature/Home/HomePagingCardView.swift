4ebeb87c5e739a6f1eb623465272caf9c82c00ec
EllGHT <rmsgh1188@gmail.com>
2025-06-14 15:01:32 +0900

    fix: #40 - 휴식 프로그레스바 로직 수정 및 구현완료

--- HowManySet/Presentation/Feature/Home/HomePagingCardView.swift
+++ HowManySet/Presentation/Feature/Home/HomePagingCardView.swift
@@ -248,7 +248,7 @@ extension HomePagingCardView {
     
     func configure(with state: WorkoutCardState) {
         
-        print("카드 뷰: \(state.currentExerciseName), \(state.currentSetNumber)")
+        print("카드 뷰: \(state.currentExerciseName), \(state.currentSetNumber)세트")
         
         exerciseNameLabel.text = state.currentExerciseName
         exerciseSetLabel.text = "\(state.currentSetNumber) / \(state.totalSetCount)"
