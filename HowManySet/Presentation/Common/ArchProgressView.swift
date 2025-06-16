6bb3d9c1ad3115c0650ec01dbc266ad799dc6aab
EllGHT <rmsgh1188@gmail.com>
2025-06-16 21:09:41 +0900

    docs: #30 - ArchProgressView 생성

---
+++ HowManySet/Presentation/Common/ArchProgressView.swift
@@ -0,0 +1,43 @@
+//
+//  ArchProgressView.swift
+//  HowManySet
+//
+//  Created by 정근호 on 6/16/25.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class ArchProgressView: UIView {
+    
+    // MARK: - Properties
+    
+    // MARK: - UI Components
+    
+    // MARK: - Initializer
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+// MARK: - UI Methods
+private extension ArchProgressView {
+    
+    func setupUI() {
+        
+    }
+    
+    func setViewHiearchy() {
+        
+    }
+    
+    func setConstraints() {
+        
+    }
+}
