a2c75601952c0c852e0140b68ce838889b9aa16a
EllGHT <rmsgh1188@gmail.com>
2025-06-16 21:03:42 +0900

    feat: #30 - 운동 완료 화면 UI 구현

--- HowManySet/Presentation/Feature/RoutineComplete/RoutineCompleteViewController.swift
+++ HowManySet/Presentation/Feature/RoutineComplete/RoutineCompleteViewController.swift
@@ -18,8 +18,117 @@ final class RoutineCompleteViewController: UIViewController, View {
     
     var disposeBag = DisposeBag()
     
-    // MARK: - UI Components
+    private let exerciseCompletedText = "운동 완료! 수고했어요"
+    private let exerciseRecordSavedText = "운동 기록 저장됨"
+    private let memoPlaceHolderText = "메모를 입력해 주세요."
+    private let confirmText = "확인"
     
+    // MARK: - UI Components
+    private lazy var topLabelVStack = UIStackView().then {
+        $0.axis = .vertical
+        $0.spacing = 14
+    }
+    
+    private lazy var exerciseCompletedLabel = UILabel().then {
+        $0.text = exerciseCompletedText
+        $0.font = .systemFont(ofSize: 24, weight: .medium)
+        $0.textColor = .white
+        $0.textAlignment = .center
+    }
+    
+    private lazy var exerciseInfoLabel = UILabel().then {
+        $0.text = "루틴명 | 2025.06.06 운동 기록 저장됨"
+        $0.font = .systemFont(ofSize: 12, weight: .regular)
+        $0.textColor = .lightGray
+        $0.textAlignment = .center
+    }
+    
+    private lazy var cardContentsContainer = UIView().then {
+        $0.backgroundColor = .cardBackground
+        $0.layer.cornerRadius = 20
+    }
+    
+    private lazy var progressView = UIImageView(image: UIImage(systemName: "rainbow")).then {
+        $0.tintColor = .brand
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    private lazy var percentageLabel = UILabel().then {
+        $0.text = "80%"
+        $0.font = .systemFont(ofSize: 36, weight: .semibold)
+        $0.textColor = .white
+        $0.textAlignment = .center
+    }
+    
+    private lazy var exerciseTimeLabel = UILabel().then {
+        $0.text = "40:38"
+        $0.font = .systemFont(ofSize: 20, weight: .regular)
+        $0.textColor = .white
+        $0.textAlignment = .center
+    }
+    
+    private lazy var exerciseAndSetInfoLabel = UILabel().then {
+        $0.text = "5개 운동, 25세트"
+        $0.font = .systemFont(ofSize: 20, weight: .regular)
+        $0.textColor = .white
+        $0.textAlignment = .center
+    }
+    
+    private lazy var timeIcon = UIImageView().then {
+        $0.image = UIImage(systemName: "clock")
+        $0.tintColor = .white
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    private lazy var exerciseIcon = UIImageView().then {
+        $0.image = UIImage(systemName: "dumbbell")
+        $0.tintColor = .white
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    private lazy var timeInfoHStack = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 16
+        $0.alignment = .leading
+    }
+    
+    private lazy var exerciseInfoHStack = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 16
+        $0.alignment = .leading
+    }
+    
+    private lazy var routineStatisticsVStack = UIStackView().then {
+        $0.axis = .vertical
+        $0.distribution = .equalSpacing
+        $0.backgroundColor = .tabBarBG
+        $0.layer.cornerRadius = 20
+    }
+    
+    private lazy var shareButton = UIButton().then {
+        $0.setPreferredSymbolConfiguration(UIImage.SymbolConfiguration(pointSize: 24), forImageIn: .normal)
+        $0.setImage(UIImage(systemName: "square.and.arrow.up"), for: .normal)
+        $0.tintColor = .white
+        $0.backgroundColor = .disabledButton
+        $0.layer.cornerRadius = 22
+    }
+    
+    private lazy var memoTextView = UITextView().then {
+        $0.backgroundColor = .black
+        $0.textColor = .placeholderText
+        $0.font = .systemFont(ofSize: 16, weight: .regular)
+        $0.layer.cornerRadius = 12
+        $0.text = memoPlaceHolderText
+        $0.textContainerInset = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+    }
+    
+    private lazy var confirmButton = UIButton().then {
+        $0.setTitle(confirmText, for: .normal)
+        $0.setTitleColor(.background, for: .normal)
+        $0.backgroundColor = .brand
+        $0.titleLabel?.font = .systemFont(ofSize: 18, weight: .medium)
+        $0.layer.cornerRadius = 12
+    }
     
     // MARK: - Initializer
     init(reactor: RoutineCompleteViewReactor, coordinator: RoutineCompleteCoordinatorProtocol) {
@@ -28,40 +137,125 @@ final class RoutineCompleteViewController: UIViewController, View {
         self.coordinator = coordinator
         
         self.hidesBottomBarWhenPushed = true
-//        self.navigationItem.hidesBackButton = true
+        self.navigationItem.hidesBackButton = true
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     // MARK: - LifeCycle
     override func viewDidLoad() {
-       
         super.viewDidLoad()
+        
+        memoTextView.delegate = self
+
+        setupUI()
     }
-    
 }
 
 // MARK: - UI Methods
 private extension RoutineCompleteViewController {
     
     func setupUI() {
+        view.backgroundColor = .background
         
+        setViewHiearchy()
+        setConstraints()
     }
     
     func setViewHiearchy() {
         
+        view.addSubviews(
+            topLabelVStack,
+            cardContentsContainer,
+            percentageLabel,
+            memoTextView,
+            confirmButton
+        )
+        
+        topLabelVStack.addArrangedSubviews(exerciseCompletedLabel, exerciseInfoLabel)
+        cardContentsContainer.addSubviews(progressView, routineStatisticsVStack, shareButton)
     }
     
     func setConstraints() {
         
+        topLabelVStack.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(52)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        cardContentsContainer.snp.makeConstraints {
+            $0.top.equalTo(topLabelVStack.snp.bottom).offset(14)
+            $0.height.equalToSuperview().multipliedBy(0.43)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        shareButton.snp.makeConstraints {
+            $0.top.trailing.equalToSuperview().inset(28)
+            $0.width.height.equalTo(44)
+        }
+        
+        progressView.snp.makeConstraints {
+            $0.centerX.equalTo(cardContentsContainer)
+            $0.top.equalToSuperview().inset(72)
+            $0.width.height.equalTo(100)
+        }
+                
+        percentageLabel.snp.makeConstraints {
+            $0.center.equalTo(cardContentsContainer)
+        }
+        
+        routineStatisticsVStack.snp.makeConstraints {
+            $0.height.equalTo(cardContentsContainer.snp.height).multipliedBy(0.31)
+            $0.horizontalEdges.equalTo(cardContentsContainer.snp.horizontalEdges).inset(28)
+            $0.bottom.equalTo(cardContentsContainer.snp.bottom).inset(28)
+        }
+        
+        timeIcon.snp.makeConstraints {
+            $0.width.height.equalTo(20)
+        }
+        
+        exerciseIcon.snp.makeConstraints {
+            $0.width.height.equalTo(20)
+        }
+        
+        memoTextView.snp.makeConstraints {
+            $0.top.equalTo(cardContentsContainer.snp.bottom).offset(16)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.height.equalTo(cardContentsContainer.snp.height).multipliedBy(0.39)
+        }
+        
+        confirmButton.snp.makeConstraints {
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(16)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.height.equalTo(56)
+        }
+    }
+}
+
+// MARK: - UITextViewDelegate
+extension RoutineCompleteViewController: UITextViewDelegate {
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.text == memoPlaceHolderText {
+            textView.text = ""
+            textView.textColor = .white
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = memoPlaceHolderText
+            textView.textColor = .placeholderText
+        }
     }
 }
 
 // MARK: - Reactor Binding
 extension RoutineCompleteViewController {
-    
+ 
     func bind(reactor: RoutineCompleteViewReactor) {
         
     }
