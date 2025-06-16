1b14bc5d28dd3e356fbde88fe4e97b207bf65c3b
UkhyunGo <dnrwk99@gmail.com>
2025-06-16 20:45:06 +0900

    feat: #59 - Firestore Workout Routine DTO 구현

--- HowManySet/Data/FSDTO/FSWorkoutRoutineDTO.swift
+++ HowManySet/Data/FSDTO/FSWorkoutRoutineDTO.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import FirebaseFirestore
+
+/// Firestore에 저장되는 운동 루틴 DTO
+struct FSWorkoutRoutineDTO: Codable, Identifiable {
+    @DocumentID var id: String?
+    let userId: String
+    let name: String
+    let workouts: [FSWorkoutDTO]
+}
