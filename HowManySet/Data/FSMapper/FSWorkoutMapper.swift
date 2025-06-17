e191777f62572b2853cc86c788a568cbbf82192c
UkhyunGo <dnrwk99@gmail.com>
2025-06-17 15:02:47 +0900

    feat: #59 - WorkoutSet Mapper 구현

--- HowManySet/Data/FSMapper/FSWorkoutMapper.swift
+++ HowManySet/Data/FSMapper/FSWorkoutMapper.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+// MARK: - WorkoutSet Mapper
+extension FSWorkoutSetDTO {
+    func toDomain() -> WorkoutSet {
+        return WorkoutSet(weight: self.weight,
+                          unit: self.unit,
+                          reps: self.reps)
+    }
+}
+
+extension WorkoutSet {
+    func toFSDTO() -> FSWorkoutSetDTO {
+        return FSWorkoutSetDTO(weight: self.weight,
+                               unit: self.unit,
+                               reps: self.reps)
+    }
+}
