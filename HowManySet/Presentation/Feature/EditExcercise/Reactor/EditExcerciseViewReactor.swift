51de804f52d3c9307ba94b2e5d136c9a8a6f9665
minjae <qlqlzbto@naver.com>
2025-06-18 01:19:56 +0900

    feat: #62 - 바인드 메서드 수정 / Action, Mutation, State 수정

--- HowManySet/Presentation/Feature/EditExcercise/Reactor/EditExcerciseViewReactor.swift
+++ HowManySet/Presentation/Feature/EditExcercise/Reactor/EditExcerciseViewReactor.swift
@@ -2,7 +2,7 @@
 //  EditExcerciseViewReactor.swift
 //  HowManySet
 //
-//  Created by 정근호 on 6/4/25.
+//  Created by MJ Dev on 6/4/25.
 //
 
 import Foundation
@@ -13,32 +13,63 @@ final class EditExcerciseViewReactor: Reactor {
     
     // Action is an user interaction
     enum Action {
-        
+        case addExcerciseButtonTapped
+        case saveRoutineButtonTapped
+        case changeExerciseName(String)
+        case changeUnit(String)
     }
     
     // Mutate is a state manipulator which is not exposed to a view
     enum Mutation {
-        
+        case addExcercise
+        case saveRoutine
+        case changeExcerciseName(String)
+        case changeUnit(String)
     }
     
     // State is a current view state
     struct State {
-        
+        var currentRoutine: WorkoutRoutine
+        var currentExcerciseName: String = ""
+        var currentUnit: String = "kg"
     }
     
     let initialState: State
     
-    init() {
-        self.initialState = State()
+    init(routineName: String) {
+        self.initialState = State(currentRoutine: WorkoutRoutine(name: routineName, workouts: []))
     }
     
-    //    // Action -> Mutation
-    //    func mutate(action: Action) -> Observable<Mutation> {
-    //
-    //    }
-    //
-    //    // Mutation -> State
-    //    func reduce(state: State, mutation: Mutation) -> State {
-    //
-    //    }
+    // Action -> Mutation
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .addExcerciseButtonTapped:
+            return .just(.addExcercise)
+        case .saveRoutineButtonTapped:
+            return .just(.saveRoutine)
+        case .changeExerciseName(let name):
+            return .just(.changeExcerciseName(name))
+        case .changeUnit(let unit):
+            return .just(.changeUnit(unit))
+        }
+    }
+    
+    // Mutation -> State
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        
+        switch mutation {
+        case .addExcercise:
+            print(newState.currentRoutine.name)
+        case .saveRoutine:
+            print(newState.currentRoutine)
+        case .changeExcerciseName(let newName):
+            print(newName)
+            newState.currentExcerciseName = newName
+        case .changeUnit(let unit):
+            print(unit)
+            newState.currentUnit = unit
+        }
+        return newState
+    }
 }
