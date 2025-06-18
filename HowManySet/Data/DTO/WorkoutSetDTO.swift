73d079b4d76f0edac18b50324f9047b5779a09ed
minjae <qlqlzbto@naver.com>
2025-06-18 19:37:31 +0900

    refactor: #73 - DTO, RealmModel 분리

--- HowManySet/Data/DTO/WorkoutSetDTO.swift
+++ HowManySet/Data/DTO/WorkoutSetDTO.swift
@@ -8,52 +8,31 @@
 import Foundation
 import RealmSwift
 
-/// 운동 세트의 정보를 나타내는 Realm 객체입니다.
-/// 무게, 단위, 반복 횟수를 포함합니다.
-final class WorkoutSetDTO: Object {
-    /// 세트에서 들어올린 무게입니다.
-    @Persisted var weight: Double
-
-    /// 무게의 단위입니다. 예: "kg", "lb"
-    @Persisted var unit: String
-
-    /// 해당 세트에서 수행한 반복 횟수입니다.
-    @Persisted var reps: Int
-
-    /// 주어진 값들로 `WorkoutSetDTO` 객체를 초기화합니다.
-    /// - Parameters:
-    ///   - weight: 들어올린 무게
-    ///   - unit: 무게의 단위
-    ///   - reps: 반복 횟수
-    convenience init(
-        weight: Double,
-        unit: String,
-        reps: Int
-    ) {
-        self.init()
-        self.weight = weight
-        self.unit = unit
-        self.reps = reps
-    }
+struct WorkoutSetDTO {
+    var weight: Double
+    var unit: String
+    var reps: Int
 }
 
 extension WorkoutSetDTO {
-    /// DTO 객체를 도메인 모델인 `WorkoutSet`으로 변환합니다.
-    /// - Returns: 동일한 값을 갖는 `WorkoutSet` 객체
     func toEntity() -> WorkoutSet {
         return WorkoutSet(weight: self.weight,
                           unit: self.unit,
                           reps: self.reps)
     }
 }
-
 extension WorkoutSetDTO {
-    /// 도메인 모델 `WorkoutSet`을 기반으로 `WorkoutSetDTO`를 초기화합니다.
-    /// - Parameter entity: 변환할 `WorkoutSet` 객체
-    convenience init(entity: WorkoutSet) {
-        self.init()
+    init(entity: WorkoutSet) {
         self.weight = entity.weight
         self.unit = entity.unit
         self.reps = entity.reps
+    }
+}
+
+extension WorkoutSetDTO {
+    init(from model: RMWorkoutSet) {
+        self.weight = model.weight
+        self.unit = model.unit
+        self.reps = model.reps
     }
 }
---
+++ HowManySet/Data/PersistentStorages/Model/RMWorkoutRoutine.swift
@@ -0,0 +1,48 @@
+//
+//  WorkoutRoutineDTO.swift
+//  HowManySet
+//
+//  Created by MJ Dev on 6/3/25.
+//
+
+import Foundation
+import RealmSwift
+
+final class RMWorkoutRoutine: Object {
+
+    @Persisted var name: String
+    @Persisted var workouts = List<RMWorkout>()
+
+    var workoutArray: [RMWorkout] {
+        get {
+            return workouts.map { $0 }
+        }
+        set {
+            workouts.removeAll()
+            workouts.append(objectsIn: newValue)
+        }
+    }
+    convenience init(
+        name: String,
+        workouts: [RMWorkout]
+    ) {
+        self.init()
+        self.name = name
+        self.workoutArray = workouts
+    }
+}
+
+extension RMWorkoutRoutine {
+    func toDTO() -> WorkoutRoutineDTO {
+        return WorkoutRoutineDTO(name: self.name,
+                              workouts: self.workouts.map { $0.toDTO() })
+    }
+}
+
+extension RMWorkoutRoutine {
+    convenience init(dto: WorkoutRoutineDTO) {
+        self.init()
+        self.name = name
+        self.workoutArray = dto.workouts.map { RMWorkout(dto: $0) }
+    }
+}
