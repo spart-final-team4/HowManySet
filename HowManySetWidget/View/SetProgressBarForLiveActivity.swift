81372232a4af6c02b91d7b731f8b0f010d75bc92
EllGHT <rmsgh1188@gmail.com>
2025-06-19 02:16:59 +0900

    refactor: #63 - setProgressBarForLiveActivity 수정

--- HowManySetWidget/View/SetProgressBarForLiveActivity.swift
+++ HowManySetWidget/View/SetProgressBarForLiveActivity.swift
@@ -1,5 +1,5 @@
 //
-//  SetProgressBar.swift
+//  SetProgressBarForLiveActivity.swift
 //  HowManySet
 //
 //  Created by 정근호 on 6/19/25.
@@ -14,13 +14,13 @@ struct SetProgressBarForLiveActivity: View {
     var body: some View {
         HStack(spacing: 2) {
             ForEach(0..<totalSets, id: \.self) { index in
-                RoundedRectangle(cornerRadius: 5)
+                Rectangle()
                     .fill(index < currentSet ? Color.brand : Color.gray)
                     .frame(height: 12)
             }
+            .background(Color("Background"))
         }
-        .padding(.horizontal, 4)
-        .background(Color.black)
-        .clipShape(RoundedRectangle(cornerRadius: 5))
+        .cornerRadius(4)
+        .background(Color("Background"))
     }
 }
