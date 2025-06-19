52ca66f60464bcae2975b263844373495506a2e6
EllGHT <rmsgh1188@gmail.com>
2025-06-19 11:31:38 +0900

    build: #63 - IntentExtension 추가

---
+++ HowManySetIntentExtension/HowManySetIntentExtensionExtension.swift
@@ -0,0 +1,12 @@
+//
+//  HowManySetIntentExtensionExtension.swift
+//  HowManySetIntentExtension
+//
+//  Created by 정근호 on 6/19/25.
+//
+
+import AppIntents
+
+@main
+struct HowManySetIntentExtensionExtension: AppIntentsExtension {
+}
