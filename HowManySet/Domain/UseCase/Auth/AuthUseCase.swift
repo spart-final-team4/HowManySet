271105d1c95ff5e8a6d38fc35fedf47a4adc4332
UkhyunGo <dnrwk99@gmail.com>
2025-06-19 15:22:02 +0900

    feat: #65 - FirebaseAuthUseCase 구현

--- HowManySet/Domain/UseCase/Auth/AuthUseCase.swift
+++ HowManySet/Domain/UseCase/Auth/AuthUseCase.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+final class AuthUseCase: AuthUseCaseProtocol {
+    private let repository: AuthRepository
+
+    init(repository: AuthRepository) {
+        self.repository = repository
+    }
+
+    func signInAnonymously(completion: @escaping (Result<User, Error>) -> Void) {
+        repository.signInAnonymously(completion: completion)
+    }
+
+    func fetchCurrentUser() -> User? {
+        return repository.fetchCurrentUser()
+    }
+
+    func signOut() -> Result<Void, Error> {
+        return repository.signOut()
+    }
+
+    func deleteAccount(completion: @escaping (Result<Void, Error>) -> Void) {
+        repository.deleteAccount(completion: completion)
+    }
+}
