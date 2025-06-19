580e5246b71b99b3deb108668c2c30d784e7b44a
UkhyunGo <dnrwk99@gmail.com>
2025-06-20 01:25:19 +0900

    fix: #65 - App, SceneDelegate 수정

--- HowManySet/App/AppDelegate.swift
+++ HowManySet/App/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  HowManySet
 //
-//  Created by 정근호 on 5/30/25.
+//  Created by GO on 5/30/25.
 //
 
 import UIKit
@@ -36,31 +36,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
-    
-    // 구글/카카오 인증 결과 처리 (iOS 13 이상, SceneDelegate 병행 사용)
+
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:] ) -> Bool {
-        // 구글 로그인 처리
         if GIDSignIn.sharedInstance.handle(url) {
             return true
         }
-        // 카카오 로그인 처리
         if AuthApi.isKakaoTalkLoginUrl(url) {
             return AuthController.handleOpenUrl(url: url)
         }
         return false
     }
-    
-    // MARK: UISceneSession Lifecycle
+
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions
     ) -> UISceneConfiguration {
-        // 기본 SceneDelegate 사용
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-    
-    func application(
-        _ application: UIApplication,
-        didDiscardSceneSessions sceneSessions: Set<UISceneSession>
-    ) {
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // 필요시 리소스 해제 등 처리
     }
 }
