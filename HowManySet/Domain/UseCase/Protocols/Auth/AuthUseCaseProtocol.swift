d5392fbcc348ab5e41a9588414db2c8917b380b5
UkhyunGo <dnrwk99@gmail.com>
2025-06-20 01:24:01 +0900

    feat: #65 - AuthUseCase, Protocol 구현

--- HowManySet/Domain/UseCase/Protocols/Auth/AuthUseCaseProtocol.swift
+++ HowManySet/Domain/UseCase/Protocols/Auth/AuthUseCaseProtocol.swift
@@ -5,11 +5,27 @@
 //  Created by GO on 6/19/25.
 //
 
-import Foundation
+import RxSwift
 
-protocol AuthUseCaseProtocol {
-    func signInAnonymously(completion: @escaping (Result<User, Error>) -> Void)
-    func fetchCurrentUser() -> User?
-    func signOut() -> Result<Void, Error>
-    func deleteAccount(completion: @escaping (Result<Void, Error>) -> Void)
+/// 인증 관련 비즈니스 로직을 정의하는 프로토콜
+/// - 다양한 로그인 방식의 비즈니스 로직을 추상화
+public protocol AuthUseCaseProtocol {
+    /// 카카오 로그인 비즈니스 로직
+    /// - Returns: 로그인된 사용자 정보 Observable
+    func loginWithKakao() -> Observable<User>
+    
+    /// 구글 로그인 비즈니스 로직
+    /// - Returns: 로그인된 사용자 정보 Observable
+    func loginWithGoogle() -> Observable<User>
+    
+    /// Apple 로그인 비즈니스 로직
+    /// - Parameters:
+    ///   - token: Apple ID 토큰
+    ///   - nonce: 보안을 위한 nonce 값
+    /// - Returns: 로그인된 사용자 정보 Observable
+    func loginWithApple(token: String, nonce: String) -> Observable<User>
+    
+    /// 익명 로그인 비즈니스 로직
+    /// - Returns: 로그인된 사용자 정보 Observable
+    func loginAnonymously() -> Observable<User>
 }
