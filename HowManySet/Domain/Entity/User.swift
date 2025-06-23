7be7cfc05ea63905cd1e04003eb1fd65c3ceccf8
UkhyunGo <dnrwk99@gmail.com>
2025-06-23 15:29:40 +0900

    feat: #92 - User 상태 구분 열거형 추가

--- HowManySet/Domain/Entity/User.swift
+++ HowManySet/Domain/Entity/User.swift
@@ -7,6 +7,13 @@
 
 import Foundation
 
+/// 사용자 상태 열거형
+public enum UserStatus {
+    case needsNickname      // 닉네임 설정 필요
+    case needsOnboarding    // 온보딩 필요
+    case complete           // 모든 설정 완료
+}
+
 /// 사용자 정보를 나타내는 도메인 모델
 /// - 앱 전체에서 사용되는 통합된 사용자 모델
 public struct User {
