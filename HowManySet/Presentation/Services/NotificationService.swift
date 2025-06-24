6598dd6358d7bb79bc00868f3186a5f8a62b3154
EllGHT <rmsgh1188@gmail.com>
2025-06-25 01:56:03 +0900

    feat: #123 - 알림 관련 메서드 구현

--- HowManySet/Presentation/Services/NotificationService.swift
+++ HowManySet/Presentation/Services/NotificationService.swift
@@ -5,4 +5,26 @@
 //  Created by 정근호 on 5/30/25.
 //
 
-import Foundation
+import UserNotifications
+
+final class NotificationService {
+    
+    static let shared = NotificationService()
+    
+    func requestNotification() { UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            print("알림 권한 granted: \(granted)")
+        }
+    }
+    
+    func sendRestFinishedNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "휴식 종료!"
+        content.body = "이제 다음 세트를 시작하세요!"
+        content.sound = .default
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "restFinished", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
+}
