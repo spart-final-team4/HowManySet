57f6eb466847aab4046712e079d0a0bd463d5655
EllGHT <rmsgh1188@gmail.com>
2025-06-25 14:50:12 +0900

    feat: #148 - HomeStartVC UI 설정, 동작 설정

--- HowManySet/Presentation/Feature/HomeStart/HomeStartViewController.swift
+++ HowManySet/Presentation/Feature/HomeStart/HomeStartViewController.swift
@@ -8,11 +8,14 @@
 import UIKit
 import SnapKit
 import Then
+import RxSwift
 
 final class HomeStartViewController: UIViewController {    
     
     // MARK: - Properties
     private weak var coordinator: HomeStartCoordinatorProtocol?
+    
+    private var disposeBag = DisposeBag()
 
     private let homeText = "홈"
     
@@ -32,6 +35,7 @@ final class HomeStartViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -39,6 +43,9 @@ final class HomeStartViewController: UIViewController {
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupUI()
+        bindUIEvents()
     }
     
 }
@@ -46,15 +53,54 @@ final class HomeStartViewController: UIViewController {
 // MARK: - UI Methods
 private extension HomeStartViewController {
     func setupUI() {
-        
+        setViewHiearchy()
+        setConstraints()
     }
     
     func setViewHiearchy() {
-        
+        view.addSubviews(
+            titleLabel,
+            routineStartCardView
+        )
     }
     
     func setConstraints() {
         
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.bottom.equalTo(routineStartCardView.snp.top).offset(-32)
+        }
+        
+        routineStartCardView.snp.makeConstraints {
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.height.equalToSuperview().multipliedBy(0.45)
+        }
+
     }
 }
 
+// MARK: - UI Events
+private extension HomeStartViewController {
+    
+    func bindUIEvents() {
+        
+        routineStartCardView.todayDateLabel.text = Date().toDateLabel()
+        
+        routineStartCardView.routineSelectButton.rx.tap
+            .throttle(.milliseconds(300), scheduler: MainScheduler.instance)
+            .bind(onNext: { [weak self] _ in
+                guard let self else { return }
+                UIView.animate(withDuration: 0.1, delay: 0, options: [.curveEaseInOut], animations: {
+                    self.routineStartCardView.routineSelectButton.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+                }, completion: { _ in
+                    UIView.animate(withDuration: 0.1, delay: 0, options: [.curveEaseInOut], animations: {
+                        self.routineStartCardView.routineSelectButton.transform = .identity
+                    }, completion: { _ in
+                        self.coordinator?.pushRoutineListView()
+                    })
+                })
+            })
+            .disposed(by: disposeBag)
+    }
+}
