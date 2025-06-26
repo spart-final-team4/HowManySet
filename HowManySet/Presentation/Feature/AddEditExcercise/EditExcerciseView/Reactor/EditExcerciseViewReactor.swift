f01048e9c6786448d4c8b651987cb1a0c0b0a285
minjae <qlqlzbto@naver.com>
2025-06-26 19:48:52 +0900

    refactor: #132 - 리액터와 텍스트필드의 직접연결 해제 (무한루프 발생 원인) / 저장버튼 클릭 시 텍스트필드 값들을 모두 불러오도록 수정

--- HowManySet/Presentation/Feature/AddEditExcercise/EditExcerciseView/Reactor/EditExcerciseViewReactor.swift
+++ HowManySet/Presentation/Feature/AddEditExcercise/EditExcerciseView/Reactor/EditExcerciseViewReactor.swift
@@ -16,14 +16,12 @@ final class EditExcerciseViewReactor: Reactor {
     private let updateWorkoutUseCase: UpdateWorkoutUseCaseProtocol
     
     enum Action {
-        case saveExcerciseButtonTapped
-        case changeExcerciseWeightSet([[String]])
+        case saveExcerciseButtonTapped((name: String, weightSet: [[String]]))
         case changeUnit(String)
     }
     
     enum Mutation {
-        case saveExcercise
-        case changeExcerciseWeightSet([[String]])
+        case saveExcercise((name: String, weightSet: [[String]]))
         case changeUnit(String)
     }
     
@@ -43,10 +41,8 @@ final class EditExcerciseViewReactor: Reactor {
     }
     func mutate(action: Action) -> Observable<Mutation> {
         switch action {
-        case .saveExcerciseButtonTapped:
-            return .just(.saveExcercise)
-        case .changeExcerciseWeightSet(let newWeightSet):
-            return .just(.changeExcerciseWeightSet(newWeightSet))
+        case .saveExcerciseButtonTapped((let newName, let newWeightSet)):
+            return .just(.saveExcercise((newName, newWeightSet)))
         case .changeUnit(let unit):
             return .just(.changeUnit(unit))
         }
@@ -55,17 +51,8 @@ final class EditExcerciseViewReactor: Reactor {
     func reduce(state: State, mutation: Mutation) -> State {
         var newState = state
         switch mutation {
-        case .saveExcercise:
+        case .saveExcercise((let newName, let newWeightSet)):
             break
-        case .changeExcerciseWeightSet(let newWeightSet):
-            var newSet: [WorkoutSet] = []
-            for i in 1..<newWeightSet.count {
-                let weightSet = newWeightSet[i]
-                newSet.append(WorkoutSet(weight: Double(weightSet[0]) ?? 0.0,
-                                         unit: currentState.currentUnit,
-                                         reps: Int(weightSet[1]) ?? 1))
-            }
-            newState.workout.sets = newSet
         case .changeUnit(let unit):
             newState.currentUnit = unit
         }
