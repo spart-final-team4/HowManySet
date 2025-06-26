7101bd3f00e9b9cadcc5e279a6ef78aaa46bb85b
EllGHT <rmsgh1188@gmail.com>
2025-06-27 08:43:15 +0900

    refactor: #156 - 현재 미사용 intent 주석 처리

--- HowManySetWidget/AppIntent/StopWorkoutIntent.swift
+++ HowManySetWidget/AppIntent/StopWorkoutIntent.swift
@@ -5,29 +5,30 @@
 //  Created by 정근호 on 5/30/25.
 //
 
-import AppIntents
-import ActivityKit
-import WidgetKit
-
-@available(iOSApplicationExtension 17.0, *)
-public struct StopWorkoutIntent: AppIntent, LiveActivityIntent {
-    public static var title: LocalizedStringResource = "운동 종료"
-    public static var description = IntentDescription("운동 종료 버튼(클릭 시 앱에서 Alert)")
-    @Parameter(title: "현재 운동 인덱스") 
-    public var index: Int?
-    
-    public init() {}
-
-    public init(index: Int?) {
-        self.index = index
-    }
-
-    public func perform() async throws -> some IntentResult {
-        
-        let sharedDefaults = UserDefaults(suiteName: "group.com.eightroutes.HowManySet")
-        sharedDefaults?.set(index, forKey: "StopWorkoutIndex")
-        sharedDefaults?.set(Date().timeIntervalSince1970, forKey: "StopWorkoutTimeStamp") 
-        sharedDefaults?.synchronize()
-        return .result()
-    }
-}
+// TODO: - 현재 LiveActivity에서 운동 종료는 미사용 (아마 추후 개선 예정)
+//import AppIntents
+//import ActivityKit
+//import WidgetKit
+//
+//@available(iOSApplicationExtension 17.0, *)
+//public struct StopWorkoutIntent: AppIntent, LiveActivityIntent {
+//    public static var title: LocalizedStringResource = "운동 종료"
+//    public static var description = IntentDescription("운동 종료 버튼(클릭 시 앱에서 Alert)")
+//    @Parameter(title: "현재 운동 인덱스") 
+//    public var index: Int?
+//    
+//    public init() {}
+//
+//    public init(index: Int?) {
+//        self.index = index
+//    }
+//
+//    public func perform() async throws -> some IntentResult {
+//        
+//        let sharedDefaults = UserDefaults(suiteName: "group.com.eightroutes.HowManySet")
+//        sharedDefaults?.set(index, forKey: "StopWorkoutIndex")
+//        sharedDefaults?.set(Date().timeIntervalSince1970, forKey: "StopWorkoutTimeStamp") 
+//        sharedDefaults?.synchronize()
+//        return .result()
+//    }
+//}
