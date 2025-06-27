ec2873791e242203576872d6bc7af48d6247f850
EllGHT <rmsgh1188@gmail.com>
2025-06-27 09:02:10 +0900

    docs: #156 - LiveActivity 리터럴 모음 파일 생성

---
+++ HowManySetWidget/LiveActivityDefaultsName.swift
@@ -0,0 +1,33 @@
+//
+//  LiveActivityDefaultsName.swift
+//  HowManySet
+//
+//  Created by 정근호 on 6/27/25.
+//
+
+import Foundation
+
+/// LiveActivity 사용 시 쓰이는 리터럴 값 모음
+final class LiveActivityDefaultsName {
+    
+    static let shared = LiveActivityDefaultsName()
+    private init() {}
+
+    let suiteName = "group.com.eightroutes.HowManySet"
+    
+    let playAndPauseIndex = "PlayAndPauseRestIndex"
+    let playAndPauseTimeStamp = "PlayAndPauseRestTimestamp"
+    
+    let setCompleteIndex = "SetCompleteIndex"
+    let setCompleteTimeStamp = "SetCompleteTimestamp"
+    
+    let skipIndex = "SkipIndex"
+    let skipTimeStamp = "SkipTimeStamp"
+}
+
+extension Notification.Name {
+    static let playAndPauseRestEvent = Notification.Name("playAndPauseRestEvent")
+    static let setCompleteEvent = Notification.Name("setCompleteEvent")
+    static let skipEvent = Notification.Name("skipEvent")
+}
+
