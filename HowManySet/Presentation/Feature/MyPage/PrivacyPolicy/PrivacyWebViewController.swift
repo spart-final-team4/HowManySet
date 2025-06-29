2a16c04e4165f543ca29a05870509ba558b4c963
UkhyunGo <dnrwk99@gmail.com>
2025-06-30 00:41:46 +0900

    feat: #186 - 네트워크가 없는 상황 대응을 위해 WebView로 구현

--- HowManySet/Presentation/Feature/MyPage/PrivacyPolicy/PrivacyWebViewController.swift
+++ HowManySet/Presentation/Feature/MyPage/PrivacyPolicy/PrivacyWebViewController.swift
@@ -5,4 +5,27 @@
 //  Created by GO on 6/30/25.
 //
 
-import Foundation
+import UIKit
+import WebKit
+
+final class PrivacyWebViewController: UIViewController {
+    private var webView: WKWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupWebView()
+        loadPrivacyHTML()
+    }
+
+    private func setupWebView() {
+        webView = WKWebView(frame: view.bounds)
+        webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(webView)
+    }
+
+    private func loadPrivacyHTML() {
+        if let url = Bundle.main.url(forResource: "privacy", withExtension: "html") {
+            webView.loadFileURL(url, allowingReadAccessTo: url)
+        }
+    }
+}
