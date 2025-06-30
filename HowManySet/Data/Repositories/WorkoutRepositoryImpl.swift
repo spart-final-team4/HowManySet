3f31f5e7c15a6f9c35f59a04ff8cf2e142c0ddf4
UkhyunGo <dnrwk99@gmail.com>
2025-06-30 22:32:06 +0900

    feat: #197 - WorkoutRepositoryImpl Firestore update 메서드 추가

--- HowManySet/Data/Repositories/WorkoutRepositoryImpl.swift
+++ HowManySet/Data/Repositories/WorkoutRepositoryImpl.swift
@@ -35,8 +35,20 @@ final class WorkoutRepositoryImpl: WorkoutRepository {
 private extension WorkoutRepositoryImpl {
     // MARK: - Workout Update
     func updateWorkoutFirebase(uid: String, workout: Workout) {
-        // TODO: 구현 필요
-        print("updateWorkoutFirebase 구현안되있음")
+        Task {
+            do {
+                let dto = WorkoutDTO(entity: workout)
+                let fsWorkout = dto.toFSModel()
+                try await firestoreService.update(
+                    id: workout.id,
+                    item: fsWorkout,
+                    type: FirestoreDataType<FSWorkout>.workout
+                )
+                print("Firestore 운동 업데이트 성공")
+            } catch {
+                print("Firestore 운동 업데이트 실패: \(error)")
+            }
+        }
     }
     
     func updateWorkoutRealm(workout: Workout) {
