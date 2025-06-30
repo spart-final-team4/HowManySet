d954d4961f785686e521dcefdfd1320fd0cad49e
minjae <qlqlzbto@naver.com>
2025-06-30 09:23:08 +0900

    refactor: #134 - 테스트 코드 제거

--- HowManySetTests/Routine/FetchRoutineUseCaseTests.swift
+++ HowManySetTests/Routine/FetchRoutineUseCaseTests.swift
@@ -11,32 +11,10 @@ import XCTest
 
 final class FetchRoutineUseCaseTests: XCTestCase {
     
-    var repository: RoutineRepository!
-    var usecase: FetchRoutineUseCase!
-    var disposeBag: DisposeBag!
-    let realmServiceStub = RealmServiceStub()
-    
     override func setUpWithError() throws {
-        repository = RoutineRepositoryImpl()
-        usecase = FetchRoutineUseCase(repository: repository)
-        disposeBag = DisposeBag()
+
     }
-    
-    func test_저장된루틴이_정상적으로불러와지는가() {
-        // given
-        let routine = WorkoutRoutine(id: "", name: "test", workouts: [])
-        realmServiceStub.create(item: RMWorkoutRoutine(dto: WorkoutRoutineDTO(entity: routine)))
-        
-        // when
-        let routines = realmServiceStub.read(type: .workoutRoutine)
-        
-        // then
-        XCTAssertTrue(!routines!.isEmpty)
-    }
-    
     override func tearDownWithError() throws {
-        repository = nil
-        usecase = nil
-        realmServiceStub.deleteAll()
+
     }
 }
