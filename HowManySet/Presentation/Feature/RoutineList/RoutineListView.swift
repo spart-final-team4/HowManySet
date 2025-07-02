06c517f862d6f95afab2d96a31c02fead79aba4d
UkhyunGo <dnrwk99@gmail.com>
2025-07-02 16:24:01 +0900

    feat: #236 - RoutineList 다국어 지원 적용

--- HowManySet/Presentation/Feature/RoutineList/RoutineListView.swift
+++ HowManySet/Presentation/Feature/RoutineList/RoutineListView.swift
@@ -42,7 +42,7 @@ private extension RoutineListView {
 
     func setAppearance() {
         titleLabel.do {
-            $0.text = "루틴 리스트"
+            $0.text = String(localized: "루틴 리스트")
             $0.font = .pretendard(size: 36, weight: .medium)
             $0.textColor = .white
         }
@@ -55,7 +55,7 @@ private extension RoutineListView {
 
         addNewRoutineButton.do {
             $0.backgroundColor = .green6
-            $0.setTitle("새 루틴 구성", for: .normal)
+            $0.setTitle(String(localized: "새 루틴 구성"), for: .normal)
             $0.titleLabel?.font = .pretendard(size: 18, weight: .regular)
             $0.setTitleColor(.background, for: .normal)
             $0.layer.cornerRadius = 12
