30cbe44a274844272eff8a360592d0eb47564ec6
UkhyunGo <dnrwk99@gmail.com>
2025-07-03 17:52:02 +0900

    fix: #257 - 로딩 상태값 추가하여 루틴 불러오기 Loading Indicator 이슈 해결

--- HowManySet/Presentation/Feature/RoutineList/Reactor/RoutineListViewReactor.swift
+++ HowManySet/Presentation/Feature/RoutineList/Reactor/RoutineListViewReactor.swift
@@ -18,11 +18,13 @@ final class RoutineListViewReactor: Reactor {
     enum Mutation {
         case updatedRoutine([WorkoutRoutine])
         case deleteRoutineAt(IndexPath)
+        case setLoading(Bool)
     }
     
     // MARK: - State is a current view state
     struct State {
         var routines: [WorkoutRoutine] = []
+        var isLoading: Bool = false
     }
     
     let initialState: State
@@ -46,13 +48,15 @@ final class RoutineListViewReactor: Reactor {
         switch action {
         case .viewWillAppear:
             let uid = FirebaseAuthService().fetchCurrentUser()?.uid
-            return fetchRoutineUseCase.execute(uid: uid)
-                .map{ Mutation.updatedRoutine($0) }
-                .asObservable()
-
+            return Observable.concat([
+                .just(.setLoading(true)),
+                fetchRoutineUseCase.execute(uid: uid)
+                    .map { Mutation.updatedRoutine($0) }
+                    .asObservable(),
+                .just(.setLoading(false))
+            ])
         case let .deleteRoutine(indexPath):
             let routine = currentState.routines[indexPath.section]
-            
             deleteRoutineUseCase.execute(uid: uid, item: routine)
             return .just(.deleteRoutineAt(indexPath))
         }
@@ -62,6 +66,8 @@ final class RoutineListViewReactor: Reactor {
     func reduce(state: State, mutation: Mutation) -> State {
         var newState = state
         switch mutation {
+        case .setLoading(let loading):
+            newState.isLoading = loading
         case .updatedRoutine(let routines):
             var newRoutines: [WorkoutRoutine] = []
             for i in 0..<routines.count {
@@ -72,13 +78,11 @@ final class RoutineListViewReactor: Reactor {
                 }
             }
             newState.routines = newRoutines
-
         case let .deleteRoutineAt(indexPath):
             var updated = state.routines
             updated.remove(at: indexPath.section)
             newState.routines = updated
         }
-
         return newState
     }
 }
