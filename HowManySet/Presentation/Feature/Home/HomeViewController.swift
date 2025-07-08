0dd2595ccd4cdf2d4586b66befc19cca4900a6f0
EllGHT <rmsgh1188@gmail.com>
2025-07-08 15:49:05 +0900

    feat: #290 - 휴식 시간 pause/play시 알림 로직 구현

--- HowManySet/Presentation/Feature/Home/HomeViewController.swift
+++ HowManySet/Presentation/Feature/Home/HomeViewController.swift
@@ -842,7 +842,7 @@ extension HomeViewController {
             cachedContentState = updated
             return updated
         }
-        .observe(on: MainScheduler.instance)
+        .debounce(.milliseconds(100), scheduler: MainScheduler.instance)
         .bind(onNext: { contentState in
             LiveActivityService.shared.update(state: contentState)
         })
