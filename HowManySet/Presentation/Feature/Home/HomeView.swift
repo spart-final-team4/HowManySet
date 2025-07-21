635a9be7a3cde6bc6b8538fc0e830d274cf2c6e3
EllGHT <rmsgh1188@gmail.com>
2025-07-21 14:53:47 +0900

    docs: #306 - HomeView 따로 생성

---
+++ HowManySet/Presentation/Feature/Home/HomeView.swift
@@ -0,0 +1,43 @@
+//
+//  HomeView.swift
+//  HowManySet
+//
+//  Created by 정근호 on 7/21/25.
+//
+
+import UIKit
+import SnapKit
+import Then
+import RxSwift
+
+final class HomeView: UIView {
+    
+    // MARK: - Properties
+    
+    // MARK: - UI Components
+    
+    // MARK: - Initializer
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+// MARK: - UI Methods
+private extension HomeView {
+    func setupUI() {
+        
+    }
+    
+    func setViewHiearchy() {
+        
+    }
+    
+    func setConstraints() {
+        
+    }
+}
