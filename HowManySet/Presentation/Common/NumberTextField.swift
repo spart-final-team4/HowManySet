29e23f43ebfc9fc76c0fd47ddc6b5f7e6ad184de
leeminjae <qlqlzbto@naver.com>
2025-09-09 05:21:24 +0900

    feat: #303 - NumberTextField AccessoryView 추가

--- HowManySet/Presentation/Common/NumberTextField.swift
+++ HowManySet/Presentation/Common/NumberTextField.swift
@@ -11,6 +11,31 @@ public class NumberTextField: UITextField {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private func setToolbar() -> UIToolbar {
+        let toolbar = UIToolbar()
+        toolbar.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 44)
+        
+        let previousButton = UIBarButtonItem(image: UIImage(systemName: "chevron.left"),
+                                             style: .plain,
+                                             target: self,
+                                             action: nil)
+        let nextButton = UIBarButtonItem(image: UIImage(systemName: "chevron.right"),
+                                         style: .plain,
+                                         target: self,
+                                         action: nil)
+        let completeButton = UIBarButtonItem(title: "완료", style: .plain, target: self, action: nil)
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
+                                            target: nil,
+                                            action: nil)
+        
+        toolbar.items = [previousButton, nextButton, flexSpace, completeButton]
+        return toolbar
+    }
+    
+    private func setAccessoryView() {
+        self.inputAccessoryView = setToolbar()
+    }
+    
     private func configure() {
         placeholder = String(localized: "입력")
         backgroundColor = .bottomSheetBG
@@ -22,5 +47,7 @@ public class NumberTextField: UITextField {
         let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: self.frame.height))
         leftView = paddingView
         leftViewMode = .always
+        
+        setAccessoryView()
     }
 }
