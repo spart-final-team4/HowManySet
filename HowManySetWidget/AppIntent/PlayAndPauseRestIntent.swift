93f7eb8c93091a47616cce0ee11f67d74c5d2b8b
EllGHT <rmsgh1188@gmail.com>
2025-09-21 22:57:39 +0900

    fix: #271 - 각 Intent에서 로직 처리하게끔 변경

--- HowManySetWidget/AppIntent/PlayAndPauseRestIntent.swift
+++ HowManySetWidget/AppIntent/PlayAndPauseRestIntent.swift
@@ -1,5 +1,5 @@
 //
-//  PlayRoutineIntent.swift
+//  PlayAndPauseRestIntent.swift
 //  HowManySet
 //
 //  Created by 정근호 on 6/2/25.
@@ -11,6 +11,7 @@ import WidgetKit
 
 @available(iOSApplicationExtension 17.0, *)
 public struct PlayAndPauseRestIntent: AppIntent, LiveActivityIntent {
+    
     public static var title: LocalizedStringResource = LocalizedStringResource("휴식 중지 재개")
     public static var description = IntentDescription(LocalizedStringResource("휴식 중지 및 재개 버튼"))
     @Parameter(title: LocalizedStringResource("현재 운동 인덱스"))
@@ -23,11 +24,34 @@ public struct PlayAndPauseRestIntent: AppIntent, LiveActivityIntent {
     }
     
     public func perform() async throws -> some IntentResult {
+
+        guard let activity = Activity<HowManySetWidgetAttributes>.activities.first else {
+            return .result()
+        }
+        
+        var updatedState = activity.content.state
+        updatedState.isRestPaused.toggle()
+    
+        // 휴식 PlayAndPause
+        if updatedState.isRestPaused { // Pause 클릭 시
+            // 남은 휴식 시간 처리
+            let elapsed = Date().timeIntervalSince(updatedState.restStartDate ?? Date())
+            updatedState.restSecondsRemaining -= Int(elapsed)
+            updatedState.restStartDate = nil
+        } else { // Play 클릭 시
+            // 휴식 시작 시간 초기화
+            updatedState.restStartDate = Date()
+        }
+                
+        // 변경된 content로 업데이트
+        let updatedContent = ActivityContent(state: updatedState, staleDate: nil)
+        await activity.update(updatedContent, alertConfiguration: nil)
+        
+        // UserDefaults 업데이트
         let sharedDefaults = UserDefaults(suiteName: LiveActivityDefaultsName.shared.appGroupID)
         sharedDefaults?.set(index, forKey: LiveActivityDefaultsName.shared.playAndPauseIndex)
         sharedDefaults?.set(Date().timeIntervalSince1970, forKey: LiveActivityDefaultsName.shared.playAndPauseTimeStamp)
-        sharedDefaults?.synchronize()
-        NotificationCenter.default.post(name: .playAndPauseRestEvent, object: nil)
+
         return .result()
     }
 }
--- HowManySetWidget/AppIntent/SkipIntent.swift
+++ HowManySetWidget/AppIntent/SkipIntent.swift
@@ -11,6 +11,7 @@ import WidgetKit
 
 @available(iOSApplicationExtension 17.0, *)
 public struct SkipIntent: AppIntent, LiveActivityIntent {
+    
     public static var title: LocalizedStringResource = LocalizedStringResource("스킵")
     public static var description = IntentDescription(LocalizedStringResource("휴식 시간 있을 시 휴식 스킵 / 휴식 시간 0일시 세트 스킵"))
     @Parameter(title: LocalizedStringResource("현재 운동 인덱스"))
@@ -23,11 +24,27 @@ public struct SkipIntent: AppIntent, LiveActivityIntent {
     }
 
     public func perform() async throws -> some IntentResult {
+
+        guard let activity = Activity<HowManySetWidgetAttributes>.activities.first else {
+            return .result()
+        }
+        
+        var updatedState = activity.content.state
+        
+        // Skip 로직 처리
+        updatedState.isResting = false
+        updatedState.restStartDate = nil
+        updatedState.workoutStartDate = Date()
+        
+        // 변경된 content로 업데이트
+        let updatedContent = ActivityContent(state: updatedState, staleDate: nil)
+        await activity.update(updatedContent, alertConfiguration: nil)
+        
+        // UserDefaults 업데이트
         let sharedDefaults = UserDefaults(suiteName: LiveActivityDefaultsName.shared.appGroupID)
         sharedDefaults?.set(index, forKey: LiveActivityDefaultsName.shared.skipIndex)
         sharedDefaults?.set(Date().timeIntervalSince1970, forKey: LiveActivityDefaultsName.shared.skipTimeStamp)
-        sharedDefaults?.synchronize()
-        NotificationCenter.default.post(name: .skipEvent, object: nil)
+
         return .result()
     }
 }
