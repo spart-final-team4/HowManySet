f9a7390a195aa76ad766845f36aeb7f7aebfac0a
EllGHT <rmsgh1188@gmail.com>
2025-09-22 17:33:43 +0900

    rename: #271 - Intent Entry 파일명 수정

---
+++ HowManySetIntentExtension/HowManySetIntentExtension.swift
@@ -0,0 +1,12 @@
+//
+//  HowManySetIntentExtension.swift
+//  HowManySetIntentExtension
+//
+//  Created by 정근호 on 6/19/25.
+//
+
+import AppIntents
+
+@main
+struct HowManySetIntentExtension: AppIntentsExtension {
+}
