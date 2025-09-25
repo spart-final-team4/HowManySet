3abbf03629e0e3ea44b1670ccb439b684d7e5de6
leeminjae <qlqlzbto@naver.com>
2025-09-25 20:57:49 +0900

    feat: #303 - 파일분리

---
+++ HowManySet/Presentation/Common/NavigatorableTextField/TextFieldNavigator.swift
@@ -0,0 +1,33 @@
+//
+//  TextFieldNavigator.swift
+//  HowManySet
+//
+//  Created by MJ on 9/25/25.
+//
+
+import UIKit
+
+final class TextFieldNavigator {
+    
+    private var textfields: [NavigatableTextField] = []
+    
+    func register(_ textfields: [NavigatableTextField]) {
+        self.textfields = textfields
+        self.textfields.forEach { $0.navigator = self }
+    }
+    
+    func didTappedPreviousButton(from textfield: NavigatableTextField) {
+        guard let index = textfields.firstIndex(of: textfield),
+              index > 0 else { return }
+        let previousTextField = textfields[index - 1]
+        
+        previousTextField.becomeFirstResponder()
+    }
+    
+    func didTappedNextButton(from textfield: NavigatableTextField) {
+        guard let index = textfields.firstIndex(of: textfield),
+              index < textfields.count - 1 else { return }
+        let nextTextField = textfields[index + 1]
+        nextTextField.becomeFirstResponder()
+    }
+}
