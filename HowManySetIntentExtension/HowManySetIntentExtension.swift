069eed162acbb9f5408474b44141622c096b801b
EllGHT <rmsgh1188@gmail.com>
2025-10-27 15:26:43 +0900

    feat: #354 - App Intent entry 파일 추가(복구)

---
+++ HowManySetIntentExtension/HowManySetIntentExtension.swift
@@ -0,0 +1,14 @@
+//
+//  HowManySetIntentExtension.swift
+//  HowManySetIntentExtension
+//
+//  Created by 정근호 on 10/27/25.
+//
+
+import Foundation
+import AppIntents
+
+// Intent Entry Point (삭제하면 안됨!)
+@main
+struct HowManySetIntentExtension: AppIntentsExtension {
+}
