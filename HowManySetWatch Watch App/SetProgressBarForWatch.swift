ef135bc4f4ee19dbbc12bf2f200315cc166d9da7
EllGHT <rmsgh1188@gmail.com>
2025-11-26 23:44:50 +0900

    feat: #369 - watch용 setProgressBar

---
+++ HowManySetWatch Watch App/SetProgressBarForWatch.swift
@@ -0,0 +1,26 @@
+//
+//  SetProgressBarForWatch.swift
+//  HowManySetWatch Watch App
+//
+//  Created by 정근호 on 11/26/25.
+//
+
+import SwiftUI
+
+struct SetProgressBarForWatch: View {
+    let totalSets: Int
+    let currentSet: Int
+
+    var body: some View {
+        HStack(spacing: 2) {
+            ForEach(0..<totalSets, id: \.self) { index in
+                Rectangle()
+                    .fill(index < currentSet ? Color.brand : Color.gray)
+                    .frame(height: 12)
+            }
+            .background(Color("Background"))
+        }
+        .cornerRadius(4)
+        .background(Color("Background"))
+    }
+}
