a39be771eec844a74e33fcf3ccfb5bfb8006ba2a
EllGHT <rmsgh1188@gmail.com>
2025-11-26 23:06:48 +0900

    chore: #369 - Watch target 추가

---
+++ HowManySetWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HowManySetWatch Watch App
+//
+//  Created by 정근호 on 11/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HowManySetWatch Watch App/HowManySetWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowManySetWatchApp.swift
+//  HowManySetWatch Watch App
+//
+//  Created by 정근호 on 11/26/25.
+//
+
+import SwiftUI
+
+@main
+struct HowManySetWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
