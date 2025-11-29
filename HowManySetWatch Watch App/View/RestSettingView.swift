2843347eb15eee146cf31702da58bfaba4525a86
EllGHT <rmsgh1188@gmail.com>
2025-11-29 14:24:32 +0900

    chore: #369 - 페이징뷰, 루틴정보(운동종목나열)뷰, 휴식설정 뷰 파일 생성

---
+++ HowManySetWatch Watch App/View/RestSettingView.swift
@@ -0,0 +1,18 @@
+//
+//  RestSettingView.swift
+//  HowManySetWatch Watch App
+//
+//  Created by 정근호 on 11/29/25.
+//
+
+import SwiftUI
+
+struct RestSettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RestSettingView()
+}
