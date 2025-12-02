7caf3a35e2b378e47006ca74b3cb0234d713a2b9
leeminjae <qlqlzbto@naver.com>
2025-12-02 15:39:03 +0900

    feat: #380 - Membership Reactor 생성

---
+++ HowManySet/Presentation/Feature/Membership/Reactor/MembershipViewReactor.swift
@@ -0,0 +1,30 @@
+//
+//  MembershipViewReactor.swift
+//  HowManySet
+//
+//  Created by MJ on 12/2/25.
+//
+
+import Foundation
+import ReactorKit
+
+final class MembershipViewReactor: Reactor {
+    
+    let initialState: State
+    
+    enum Action {
+        
+    }
+    
+    enum Mutation {
+        
+    }
+    
+    struct State {
+        
+    }
+    
+    init() {
+        self.initialState = State()
+    }
+}
