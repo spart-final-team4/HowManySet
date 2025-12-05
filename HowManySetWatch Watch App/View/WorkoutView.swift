4162fab9d595b556d91e07936bc2fb74a541d3e9
EllGHT <rmsgh1188@gmail.com>
2025-12-05 13:45:43 +0900

    feat: #369 - 운동 중 세트완료 로직 추가

--- HowManySetWatch Watch App/View/WorkoutView.swift
+++ HowManySetWatch Watch App/View/WorkoutView.swift
@@ -16,12 +16,13 @@ struct WorkoutView: View {
     @State private var isWorkingout: Bool = true
     @State private var isWorkoutPaused: Bool = false
     @State private var currentSet = 0
+    @State private var isWorkoutFinished = false
     
     @Binding var isResting: Bool
     @Binding var restStartDate: Date?
     
     private var currentSetInfoText: String {
-        guard !workout.sets.isEmpty, currentSet >= 0, currentSet <=
+        guard !workout.sets.isEmpty, currentSet >= 0, currentSet <
                 workout.sets.count else {
             return "세트 정보 없음"
         }
@@ -53,17 +54,34 @@ struct WorkoutView: View {
             
             VStack {
                 Button {
-                    print("세트완료 클릭")
-                    isResting.toggle()
-                    restStartDate = Date.now
+                    if isWorkoutFinished {
+                        // TODO: 운동 완료 처리
+                    } else {
+                        if currentSet < workout.sets.count - 1 {
+                            // 마지막 세트가 아님
+                            currentSet += 1
+                            isResting = true
+                            restStartDate = Date.now
+                        } else if currentSet == workout.sets.count - 1 {
+                            // 마지막 세트 완료
+                            currentSet += 1
+                            isWorkoutFinished = true
+                        }
+                    }
                 } label: {
-                    Image(systemName: "checkmark")
-                        .foregroundStyle(.white)
-                        .fontWeight(.bold)
-                        .font(.system(size: 20))
+                    if isWorkoutFinished {
+                        Text("완료")
+                            .font(.custom(pretendard.pretendardMedium, size: 14))
+                            .foregroundStyle(.white)
+                    } else {
+                        Image(systemName: "checkmark")
+                            .foregroundStyle(.white)
+                            .fontWeight(.bold)
+                            .font(.system(size: 20))
+                    }
                 }
                 .frame(width: buttonSize, height: buttonSize)
-                .background(Circle().fill(.green6))
+                .background(Circle().fill(isWorkoutFinished ? .blue : .green6))
                 .buttonStyle(.borderless)
             }
         }
